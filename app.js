fe559ad9efbb084608a1aa062d39103899ab3e3e
David Wesley <davidwesleydesenvolvedor@gmail.com>
2025-04-09 19:12:38 -0300

    Sistema de media escolar
    Atualização do sistema de media escolar.
    situação : concluído.

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ let notaBimestre2 = Number(prompt('Qual sua nota do Segundo Bimestre: ' ));
 let notaBimestre3 = Number(prompt('Qual sua nota do Terceiro Bimestre: ' ));
 let notaBimestre4 = Number(prompt('Qual sua nota do Quarto Bimestre: '));
 
-let media = (parseFloat(notaBimestre1) + parseFloat(notaBimestre2) + parseFloat(notaBimestre3) + parseFloat(notaBimestre4) /4)
+let soma = notaBimestre1 + notaBimestre2 + notaBimestre3 + notaBimestre4;
+let media = soma /4;
 
-if(media >= 6){
-    alert('Parabens! Você foi aprovado! sua media foi de ' + media)
+if(media >= 7){
+    alert('Parabens! Você foi aprovado! sua media foi de ' + media.toFixed(2))
 } else{
-    alert('Que pena! Você foi reprovado')
+    alert('Que pena! Você foi reprovado ' + media.toFixed(2));
 }
